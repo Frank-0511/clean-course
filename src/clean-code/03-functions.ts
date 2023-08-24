3bee76b2c5c81377c9eeada99b7fc57564196992
Frank Villanueva <frank05111996@gmail.com>
2023-08-24 15:17:43 -0500

    refactor getPayAmount

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -36,4 +36,19 @@
     console.log("Crear actor");
     return true;
   }
+
+  // Continuar
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }) => {
+    if (isDead) return 1500;
+
+    if (isSeparated) return 2500;
+
+    return isRetired ? 3000 : 4000;
+  };
+
+  getPayAmount({});
 })();
